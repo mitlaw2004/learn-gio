4496af446ca4a04852b41dee3dd343baee071b5f
mitlaw2004 <66900627+mitlaw2004@users.noreply.github.com>
2025-04-29 22:49:47 +0300

    Update loop.go

--- qapp/loop.go
+++ qapp/loop.go
@@ -1,42 +1,31 @@
-// SPDX-License-Identifier: Unlicense OR MIT
-
-package qapp
+package main
 
 import (
 	"image"
 	"log"
 	"os"
 
-	"gioui.org/app"       // app contains Window handling.
-	"gioui.org/io/event"  // event contains general event information.
-	"gioui.org/io/system" // system is used for system events (e.g. closing the window).
-	"gioui.org/layout"    // layout is used for layouting widgets.
-	"gioui.org/op"        // op is used for recording different operations.
-	"gioui.org/unit"      // unit contains metric conversion
+	"gioui.org/app"
+	"gioui.org/layout"
+	"gioui.org/op"
+	"gioui.org/unit"
 )
 
-// Render is a utility to start a rendering gio app.
+// Render is a utility to start a rendering Gio app.
 func Render(fn func(ops *op.Ops)) {
 	go func() {
-		w := app.NewWindow()
-		// ops will be used to encode different operations.
+		w := new(app.Window)
 		var ops op.Ops
 
-		// listen for events happening on the window.
-		for e := range w.Events() {
-			// detect the type of the event.
+		for {
+			e := w.Event()
 			switch e := e.(type) {
-			// this is sent when the application should re-render.
-			case system.FrameEvent:
-				// gtx is used to pass around rendering and event information.
-				gtx := layout.NewContext(&ops, e)
-				// render content
+			case app.FrameEvent:
+				gtx := app.NewContext(&ops, e)
 				fn(gtx.Ops)
-				// render and handle the operations from the UI.
 				e.Frame(gtx.Ops)
 
-			// this is sent when the application is closed.
-			case system.DestroyEvent:
+			case app.DestroyEvent:
 				if e.Err != nil {
 					log.Println(e.Err)
 					os.Exit(1)
@@ -48,28 +37,26 @@ func Render(fn func(ops *op.Ops)) {
 	app.Main()
 }
 
-// Input is a utility to start a rendering and input gio app.
-func Input(fn func(ops *op.Ops, queue event.Queue)) {
+// Input is a utility to start a rendering and input Gio app.
+func Input(fn func(ops *op.Ops)) {
+	Render(fn)
+}
+
+// InputSize is a utility to start a rendering and input Gio app with window size.
+func InputSize(fn func(ops *op.Ops, windowSize image.Point)) {
 	go func() {
-		w := app.NewWindow()
-		// ops will be used to encode different operations.
+		w := new(app.Window)
 		var ops op.Ops
 
-		// listen for events happening on the window.
-		for e := range w.Events() {
-			// detect the type of the event.
+		for {
+			e := w.Event()
 			switch e := e.(type) {
-			// this is sent when the application should re-render.
-			case system.FrameEvent:
-				// gtx is used to pass around rendering and event information.
-				gtx := layout.NewContext(&ops, e)
-				// render content
-				fn(gtx.Ops, gtx.Queue)
-				// render and handle the operations from the UI.
+			case app.FrameEvent:
+				gtx := app.NewContext(&ops, e)
+				fn(gtx.Ops, gtx.Constraints.Max)
 				e.Frame(gtx.Ops)
 
-			// this is sent when the application is closed.
-			case system.DestroyEvent:
+			case app.DestroyEvent:
 				if e.Err != nil {
 					log.Println(e.Err)
 					os.Exit(1)
@@ -81,28 +68,21 @@ func Input(fn func(ops *op.Ops, queue event.Queue)) {
 	app.Main()
 }
 
-// InputSize is a utility to start a rendering and input gio app.
-func InputSize(fn func(ops *op.Ops, queue event.Queue, windowSize image.Point)) {
+// Metric is a utility to start a rendering, input and metrics Gio app.
+func Metric(fn func(ops *op.Ops, metric unit.Metric)) {
 	go func() {
-		w := app.NewWindow()
-		// ops will be used to encode different operations.
+		w := new(app.Window)
 		var ops op.Ops
 
-		// listen for events happening on the window.
-		for e := range w.Events() {
-			// detect the type of the event.
+		for {
+			e := w.Event()
 			switch e := e.(type) {
-			// this is sent when the application should re-render.
-			case system.FrameEvent:
-				// gtx is used to pass around rendering and event information.
-				gtx := layout.NewContext(&ops, e)
-				// render content
-				fn(gtx.Ops, gtx.Queue, gtx.Constraints.Max)
-				// render and handle the operations from the UI.
+			case app.FrameEvent:
+				gtx := app.NewContext(&ops, e)
+				fn(gtx.Ops, gtx.Metric)
 				e.Frame(gtx.Ops)
 
-			// this is sent when the application is closed.
-			case system.DestroyEvent:
+			case app.DestroyEvent:
 				if e.Err != nil {
 					log.Println(e.Err)
 					os.Exit(1)
@@ -114,61 +94,21 @@ func InputSize(fn func(ops *op.Ops, queue event.Queue, windowSize image.Point)) 
 	app.Main()
 }
 
-// Metric is a utility to start a rendering, input and metrics gio app.
-func Metric(fn func(ops *op.Ops, queue event.Queue, metric unit.Metric)) {
-	go func() {
-		w := app.NewWindow()
-		// ops will be used to encode different operations.
-		var ops op.Ops
-
-		// listen for events happening on the window.
-		for e := range w.Events() {
-			// detect the type of the event.
-			switch e := e.(type) {
-			// this is sent when the application should re-render.
-			case system.FrameEvent:
-				// gtx is used to pass around rendering and event information.
-				gtx := layout.NewContext(&ops, e)
-				// render content
-				fn(gtx.Ops, gtx.Queue, gtx.Metric)
-				// render and handle the operations from the UI.
-				e.Frame(gtx.Ops)
-
-			// this is sent when the application is closed.
-			case system.DestroyEvent:
-				if e.Err != nil {
-					log.Println(e.Err)
-					os.Exit(1)
-				}
-				os.Exit(0)
-			}
-		}
-	}()
-	app.Main()
-}
-
-// Layout is a utility to start a layouting gio app.
+// Layout is a utility to start a layouting Gio app.
 func Layout(lay func(gtx layout.Context) layout.Dimensions) {
 	go func() {
-		w := app.NewWindow()
-		// ops will be used to encode different operations.
+		w := new(app.Window)
 		var ops op.Ops
 
-		// listen for events happening on the window.
-		for e := range w.Events() {
-			// detect the type of the event.
+		for {
+			e := w.Event()
 			switch e := e.(type) {
-			// this is sent when the application should re-render.
-			case system.FrameEvent:
-				// gtx is used to pass around rendering and event information.
-				gtx := layout.NewContext(&ops, e)
-				// render content
+			case app.FrameEvent:
+				gtx := app.NewContext(&ops, e)
 				lay(gtx)
-				// render and handle the operations from the UI.
 				e.Frame(gtx.Ops)
 
-			// this is sent when the application is closed.
-			case system.DestroyEvent:
+			case app.DestroyEvent:
 				if e.Err != nil {
 					log.Println(e.Err)
 					os.Exit(1)
